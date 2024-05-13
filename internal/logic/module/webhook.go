79e7dddede6227ff62926e7b95de567883a45f67
yzy613 <59520517+yzy613@users.noreply.github.com>
2024-05-13 21:07:30 +0800

    feat(webhook): supports modify UA

--- internal/logic/module/webhook.go
+++ internal/logic/module/webhook.go
@@ -9,10 +9,10 @@ import (
 func (s *sModule) WebhookGetHeadConnectOptionsTrace(ctx context.Context, header, method, url string) (
 	statusCode int, contentType string, body []byte, err error) {
 	c := gclient.New()
+	c.SetAgent(consts.ProjName + "/" + consts.Version)
 	if header != "" {
 		c.SetHeaderRaw(header)
 	}
-	c.SetAgent(consts.ProjName + "/" + consts.Version)
 	resp, err := c.DoRequest(ctx, method, url)
 	if err != nil || resp == nil {
 		return
@@ -27,10 +27,10 @@ func (s *sModule) WebhookGetHeadConnectOptionsTrace(ctx context.Context, header,
 func (s *sModule) WebhookPostPutPatchDelete(ctx context.Context, header, method, url string, payload any) (
 	statusCode int, contentType string, body []byte, err error) {
 	c := gclient.New()
+	c.SetAgent(consts.ProjName + "/" + consts.Version)
 	if header != "" {
 		c.SetHeaderRaw(header)
 	}
-	c.SetAgent(consts.ProjName + "/" + consts.Version)
 	resp, err := c.DoRequest(ctx, method, url, payload)
 	if err != nil || resp == nil {
 		return
