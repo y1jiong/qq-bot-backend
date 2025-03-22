ed68f65d5e19e4f38b8076f70d0c9c6ae58bba63
y1jiong <59520517+y1jiong@users.noreply.github.com>
2025-03-23 03:45:49 +0800

    chore: deps up: sonic

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.24
 
 require (
 	github.com/bitly/go-simplejson v0.5.1
-	github.com/bytedance/sonic v1.13.1
+	github.com/bytedance/sonic v1.13.2
 	github.com/gogf/gf/contrib/drivers/pgsql/v2 v2.9.0
 	github.com/gogf/gf/contrib/trace/otlpgrpc/v2 v2.9.0
 	github.com/gogf/gf/v2 v2.9.0
--- internal/logic/event/keyword_reply.go
+++ internal/logic/event/keyword_reply.go
@@ -312,7 +312,7 @@ func (s *sEvent) keywordReplyRewrite(ctx context.Context,
 	// 防止循环递归
 	if err := service.Bot().SetHistory(ctx, hit); err != nil {
 		// rewrite loop detected
-		g.Log().Notice(ctx, "rewrite loop detected: "+hit)
+		g.Log().Notice(ctx, "rewrite loop detected:", hit)
 		return
 	}
 	// 解码提取
